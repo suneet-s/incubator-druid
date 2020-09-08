6bc065cb0f550198004d75717315e0e8f25a55bc
Suneet Saldanha <suneet.saldanha@imply.io>
2020-09-08 11:02:20 -0700

    StringFirstAggregator and StringGroupByColumnSelectorStrategy bug fixes (#197)
    * StringFirstAggregatorFactory: Fix incorrect "combine" method. (#10351)
    
    * StringFirstAggregatorFactory: Fix incorrect "combine" method.
    
    There was a test, but it was wrong.
    
    * Fix superclass.
    
    * fix NPE in StringGroupByColumnSelectorStrategy#bufferComparator (#10325)
    
    * fix NPE in StringGroupByColumnSelectorStrategy#bufferComparator
    
    * Add tests
    
    * javadocs
    
    Co-authored-by: Gian Merlino <gianmerlino@gmail.com>

--- processing/src/main/java/org/apache/druid/query/aggregation/first/StringFirstAggregatorFactory.java
+++ processing/src/main/java/org/apache/druid/query/aggregation/first/StringFirstAggregatorFactory.java
@@ -187,7 +187,7 @@ public class StringFirstAggregatorFactory extends AggregatorFactory
   @Override
   public Object combine(Object lhs, Object rhs)
   {
-    return TIME_COMPARATOR.compare(lhs, rhs) > 0 ? lhs : rhs;
+    return TIME_COMPARATOR.compare(lhs, rhs) <= 0 ? lhs : rhs;
   }
 
   @Override
